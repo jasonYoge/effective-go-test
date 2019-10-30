a4607f93156057c37470755067935f4e8988024b
jasonYoge <jasonYoga@github.com>
2019-10-30 22:55:23 +0800

    feat: add file to reflect

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
-import "fmt"
+type square struct {
+	Edge int
+}
+
 
 func main() {
-	aMap := map[string]int{}
 
-	aMap["test"] = 1
-
-	fmt.Println(aMap)
-
-	fmt.Println(len(aMap))
-
-	aMap = make(map[string]int, 5)
-
-	fmt.Println(aMap)
 }
